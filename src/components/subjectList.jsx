49efaa225f56bd92f85f9f0122c26dd1b650a3a8
Portnov Nicolay <nicksleap@gmail.com>
2019-12-05 01:10:08 +0400

    stage

---
+++ src/components/subjectList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Square from './square'
+
+class SubjectList extends React.Component {
+  render() {
+    return(
+      <Square {...this.peops} />
+    )
+  }
+}
+
+
+export default SubjectList
